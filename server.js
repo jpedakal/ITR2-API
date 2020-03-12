37721b047824036d7c4ca6ebce1e28c5ed75e0bf
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-03-12 23:03:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,9 @@ const webapi = require('./routes/api');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/',webapi);
+app.use('/', webapi);
 
 const Port = process.env.PORT || 3000;
 app.listen(Port, () => {
-  console.log(`server running on port ${Port}`)
+    console.log(`server running on port ${Port}`)
 });
-
-
-
-
-
